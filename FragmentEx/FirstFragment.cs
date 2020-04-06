6ad004c0caec87341e205be054742c6558a0f43e
sidhuraj <mr.prvsantosh@rediffmail.com>
2020-04-06 15:18:24 +0530

    first commit

---
+++ FragmentEx/FirstFragment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace FragmentEx
+{
+    public class FirstFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            View rootView = inflater.Inflate(Resource.Layout.firstlayout, container, false);
+
+
+            Button btnFirst = rootView.FindViewById<Button>(Resource.Id.btnFirst);
+            btnFirst.Click += delegate
+            {
+                Toast.MakeText(Activity, "this is button click event", ToastLength.Long).Show();
+            };
+
+
+
+
+            return rootView;
+        }
+    }
+}
---
+++ FragmentEx/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace FragmentEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+
+            FirstFragment objFist = new FirstFragment();
+
+            FragmentManager fm = FragmentManager;
+            FragmentTransaction ft = fm.BeginTransaction();
+
+            ft.Add(Resource.Id.flContainer, objFist);
+
+
+            SecondFragment second = new SecondFragment();
+            ft.Add(Resource.Id.flContainer1, second);
+
+            ft.CommitAllowingStateLoss();
+
+        }
+    }
+}
